f0fba41542bbb4fec652eb9a5bccc1953e2fb977
DarshanaUOP <46531949+DarshanaUOP@users.noreply.github.com>
2020-05-07 13:00:56 +0530

    read responce from comment processer and get responce to the page

--- process.php
+++ process.php
@@ -7,57 +7,57 @@
 
  	$sendObj = new \stdClass();
 	if (isset($_POST['name']) && $_POST['comment']) {
-		
 
-	   $myFile = "comments.json";
-	   $arr_data = array(); // create empty array
-	try{
-		   //Get form data
-		   $formdata = array(
-		      'name'=> $_POST['name'],
-		      'comment'=> $_POST['comment'],
-		      'date' =>$dateAndTime,
-		      'ip' => $ipAddress,
-		      'browser' => $browser
-		   );
+		   $myFile = "comments.json";
+		   $arr_data = array(); // create empty array
+		try{
+			   //Get form data
+			   $formdata = array(
+			      'name'=> $_POST['name'],
+			      'comment'=> $_POST['comment'],
+			      'date' =>$dateAndTime,
+			      'ip' => $ipAddress,
+			      'browser' => $browser
+			   );
 
-		   //Get data from existing json file
-		   $jsondata = file_get_contents($myFile);
+			   //Get data from existing json file
+			   $jsondata = file_get_contents($myFile);
 
-		   // converts json data into array
-		   $arr_data = json_decode($jsondata, true);
+			   // converts json data into array
+			   $arr_data = json_decode($jsondata, true);
 
-		   print_r($formdata);
+			   // print_r($formdata);
 
-		   // Push user data to array
-		   // array_push($arr_data,$formdata);
-			$arr_data[] = $formdata;
-	       //Convert updated array to JSON
-		   $jsondata = json_encode($arr_data, JSON_PRETTY_PRINT);
-		   
-		   //write json data into data.json file
-		   if(file_put_contents($myFile, $jsondata)) {
-		        $sendObj->massage = "Done";
-		        $sendObj->user = $_POST['name'];
-		        $sendObj->comment = $_POST['comment'];
-		        $sendObj->date = $dateAndTime;
-		    }
-		   else 
-		        // echo "error";
-		    $sendObj->massage = "Error";
-			$sendObj->cause = "Unable to save data on server";
+			   // Push user data to array
+			   // array_push($arr_data,$formdata);
+				$arr_data[] = $formdata;
+		       //Convert updated array to JSON
+			   $jsondata = json_encode($arr_data, JSON_PRETTY_PRINT);
+			   
+			   //write json data into data.json file
+			   if(file_put_contents($myFile, $jsondata)) {
+			        $sendObj->massage = "Done";
+			        $sendObj->user = $_POST['name'];
+			        $sendObj->comment = $_POST['comment'];
+			        $sendObj->date = $dateAndTime;
+			    }
+			   else 
+			        // echo "error";
+			    $sendObj->massage = "Error";
+				$sendObj->cause = "Unable to save data on server";
 
-	   }
-	   catch (Exception $e) {
-		    $sendObj->massage = "Error";
-			$sendObj->cause = "Exception occured " . $e->getMessage();
+		   }
+		   catch (Exception $e) {
+			    $sendObj->massage = "Error";
+				$sendObj->cause = "Exception occured " . $e->getMessage();
 
-            // echo 'Caught exception: ',  $e->getMessage(), "\n";
-	   }		
-	}else{
-		$sendObj->massage = "Error";
-		$sendObj->cause = "no necessary data";
+	            // echo 'Caught exception: ',  $e->getMessage(), "\n";
+		   }	
+		}else{
+			$sendObj->massage = "Error";
+			$sendObj->cause = "no necessary data";
 
-	}
+		}
+		die(json_encode($sendObj));
 	
  ?>
